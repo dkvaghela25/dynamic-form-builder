34dd0b86c2a297f27648e457970df7e62248d4e2
Divyang Vaghela <dkvaghela724@gmail.com>
2026-02-27 10:07:00 +0530

    Made some changes in  icons componenent

--- src/components/ui/Icon.jsx
+++ src/components/ui/Icon.jsx
@@ -2,11 +2,23 @@ import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 
 const Icon = ({ icon, onClick }) => {
+
+    const getDynamicStyles = () => {
+        switch (icon) {
+            case "edit": return "text-emerald-600 hover:bg-emerald-50";
+            case "delete": return "text-red-600 hover:bg-red-50";
+        }
+    }
+
+    const icons = {
+        edit: <FaEdit />,
+        delete: <MdDelete />
+    }
+
     return (
-        <>
-            {icon === "edit" && <FaEdit className="h-5 w-5 cursor-pointer text-emerald-500 transition hover:text-emerald-700" onClick={onClick} />}
-            {icon === "delete" && <MdDelete className="h-5 w-5 cursor-pointer text-red-500 transition hover:text-rose-700" onClick={onClick} />}
-        </>
+        <button className={`flex h-8 w-8 items-center justify-center rounded-lg border border-slate-200 bg-white cursor-pointer ${getDynamicStyles()}`} onClick={onClick}>
+            {icons[icon]}
+        </button>
     );
 };
 
