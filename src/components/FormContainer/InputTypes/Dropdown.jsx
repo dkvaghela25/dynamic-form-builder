1311db65e040b9bdfb9ef4bc67005f34cb9fd8e6
Divyang Vaghela <dkvaghela724@gmail.com>
2026-03-06 09:35:48 +0530

    created custom ChevronDown for select tag

--- src/components/FormContainer/InputTypes/Dropdown.jsx
+++ src/components/FormContainer/InputTypes/Dropdown.jsx
@@ -1,4 +1,5 @@
 import { useCurrentSchemaContext } from "../../../contexts/CurrentSchemaContext";
+import { GoChevronDown } from "react-icons/go";
 
 const Dropdown = ({ field, error, handleChange }) => {
 
@@ -6,17 +7,22 @@ const Dropdown = ({ field, error, handleChange }) => {
     const { placeholder, value, options } = schema;
 
     return (
-        <select
-            {...field}
-            onChange={handleChange}
-            defaultValue=""
-            className={`w-full rounded-xl border ${value === "" ? "text-slate-300" : "text-slate-900"} ${error ? "border-red-300" : "border-slate-300"} bg-white px-3 py-2.5 outline-none transition placeholder:text-slate-400 focus:border-indigo-400 focus:ring-2 focus:ring-indigo-100`}
-        >
-            <option className="text-slate-900" disabled value="">{placeholder || "Select Option"}</option>
-            {options.map(({ label, value }) => {
-                return <option className="text-slate-900" key={label} value={value}>{label}</option>
-            })}
-        </select>
+        <div className="relative w-full">
+            <select
+                {...field}
+                onChange={handleChange}
+                defaultValue=""
+                className={`w-full pr-10 appearance-none rounded-xl border ${value === "" ? "text-slate-300" : "text-slate-900"} ${error ? "border-red-300" : "border-slate-300"} bg-white px-3 py-2.5 outline-none transition placeholder:text-slate-400 focus:border-indigo-400 focus:ring-2 focus:ring-indigo-100`}
+            >
+                <option className="text-slate-900" disabled value="">{placeholder || "Select Option"}</option>
+                {options.map(({ label, value }) => {
+                    return <option className="text-slate-900" key={label} value={value}>{label}</option>
+                })}
+            </select>
+            <span className="flex justify-end items-center h-0 text-slate-500 pointer-events-none absolute top-6 right-3">
+                <GoChevronDown className="absolute h-6 w-6" />
+            </span>
+        </div>
     );
 };
 
