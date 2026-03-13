171d4e9a37890524701677d03b12e5b3eb6a2a80
Divyang Vaghela <dkvaghela724@gmail.com>
2026-03-13 10:59:05 +0530

    Implemented drag and drop functionality

--- src/components/MainContent/EditForm/EditForm.jsx
+++ src/components/MainContent/EditForm/EditForm.jsx
@@ -1,24 +1,54 @@
 import { useState } from "react";
-import { useFormSchema } from "../../../contexts/formSchemaContext";
+import { useFormSchema, useSetFormSchema } from "../../../contexts/formSchemaContext";
 import InputCard from "./InputCard/InputCard";
+import { arrayMove, SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
+import { DndContext, PointerSensor, useSensor, useSensors } from "@dnd-kit/core";
 
 const EditForm = () => {
 
     const formSchema = useFormSchema();
+    const setFormSchema = useSetFormSchema();
     const [displayId, setDisplayId] = useState(false);
 
     const handleToggle = (index) => {
         setDisplayId(prev => prev === index ? false : index)
     }
 
+    const itemIds = formSchema.map((_, i) => i.toString());
+
+    const sensors = useSensors(
+        useSensor(PointerSensor, {
+            activationConstraint: {
+                distance: 8,
+            },
+        })
+    );
+
+
+    const handleDragEnd = (e) => {
+        const { active, over } = e;
+        if (!over || active.id === over.id) return;
+
+        setFormSchema((prev) => {
+            const oldIndex = parseInt(active.id);
+            const newIndex = parseInt(over.id);
+            return arrayMove(prev, oldIndex, newIndex);
+        });
+    };
+
     return (
-        <>
-            {formSchema.map((schema, index) => {
-                return (
-                    <InputCard key={index} displayId={displayId} handleToggle={handleToggle} schema={schema} index={index} />
-                )
-            })}
-        </>
+        <DndContext sensors={sensors} onDragEnd={handleDragEnd} >
+            <SortableContext
+                strategy={verticalListSortingStrategy}
+                items={itemIds}
+            >
+                {formSchema.map((schema, index) => {
+                    return (
+                        <InputCard key={index.toString()} displayId={displayId} handleToggle={handleToggle} schema={schema} id={index.toString()} />
+                    )
+                })}
+            </SortableContext>
+        </DndContext>
     );
 };
 
--- src/components/MainContent/EditForm/InputCard/InputCard.jsx
+++ src/components/MainContent/EditForm/InputCard/InputCard.jsx
@@ -6,10 +6,16 @@ import { useSetFormSchema } from "../../../../contexts/formSchemaContext";
 import { GoChevronDown } from "react-icons/go";
 import Icon from "../../../ui/Icon";
 import { RxDragHandleDots2 } from "react-icons/rx";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 const EditSchemaForm = lazy(() => import("./EditSchemaForm/EditSchemaForm"))
 
-const InputCard = ({ displayId, handleToggle, schema, index }) => {
+const InputCard = ({ displayId, handleToggle, schema, id }) => {
+
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
+
+    const index = Number(id);
 
     const [editMode, setEditMode] = useState(false);
     const { unregister } = useFormContext();
@@ -41,21 +47,37 @@ const InputCard = ({ displayId, handleToggle, schema, index }) => {
 
     return (
         <CurrentSchemaContext.Provider value={value}>
-            <div key={index} className="rounded-xl border border-slate-200 bg-slate-100 p-5 shadow-sm">
-
-                <div onClick={() => handleToggle(index)} className={`cursor-pointer flex justify-between items-center gap-2 ${displayId === index ? "mb-2 border-b pb-3" : ""} border-b-slate-300`}>
-
-                    <div className="flex items-center gap-2 font-medium text-[18px] text-slate-700 h-fit">
-                        {displayId !== index && <RxDragHandleDots2 className="-ml-2 hover:cursor-grab" />}
-                        <span>Label : {label}</span>
-                    </div>
-                    {displayId !== index
-                        ? <GoChevronDown className="w-7 h-7" />
-                        : <div className="flex gap-2">
-                            <Icon icon="edit" helperText="Edit Schema" onClick={editSchema} />
-                            <Icon icon="delete" helperText="Remove Schema" onClick={removeSchema} />
-                        </div>
+            <div
+                ref={setNodeRef}
+                style={{
+                    transform: CSS.Transform.toString(transform),
+                    transition
+                }}
+                className="rounded-xl border border-slate-200 bg-slate-100 p-5 shadow-sm"
+            >
+                <div className="flex gap-2 w-full">
+                    {displayId !== index &&
+                        <button
+                            {...attributes}
+                            {...listeners}
+                            type="button"
+                        >
+                            <RxDragHandleDots2 className="-ml-2 hover:cursor-grab" />
+                        </button>
                     }
+
+                    <div onClick={() => handleToggle(index)} className={`w-full cursor-pointer flex justify-between items-center ${displayId === index ? "mb-2 border-b pb-3" : ""} border-b-slate-300`}>
+                        <div className="font-medium text-[18px] text-slate-700 h-fit">
+                            Label : {label}
+                        </div>
+                        {displayId !== index
+                            ? <GoChevronDown className="w-7 h-7" />
+                            : <div className="flex gap-2">
+                                <Icon icon="edit" helperText="Edit Schema" onClick={editSchema} />
+                                <Icon icon="delete" helperText="Remove Schema" onClick={removeSchema} />
+                            </div>
+                        }
+                    </div>
                 </div>
 
                 {displayId === index &&
