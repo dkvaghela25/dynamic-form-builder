9eb8e57e1d977065e01183108159915c2a52fe85
Divyang Vaghela <dkvaghela724@gmail.com>
2026-03-18 17:18:32 +0530

    Changed header for table

--- src/pages/DisplayData.jsx
+++ src/pages/DisplayData.jsx
@@ -3,7 +3,7 @@ import CustomTable from "../components/DisplayData/CustomTable";
 const DisplayData = () => {
     return (
         <div className="flex flex-col items-center gap-5 mx-auto w-[90vw] bg-white py-20 rounded-2xl min-h-[90vh]">
-            <h2 className="text-2xl underline text-slate-900 font-semibold">Summary Report</h2>
+            <h2 className="text-2xl underline font-semibold">Submission Details</h2>
             <CustomTable />
         </div>
     );
