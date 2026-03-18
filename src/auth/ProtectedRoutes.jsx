4ab9cda9e9c377c1c7a2009f390d88c791bb327d
Divyang Vaghela <dkvaghela724@gmail.com>
2026-03-18 18:30:24 +0530

    Fixed issue of protected routes

--- src/auth/ProtectedRoutes.jsx
+++ src/auth/ProtectedRoutes.jsx
@@ -9,30 +9,34 @@ const ProtectedRoutes = () => {
 
     const formSchema = JSON.parse(localStorage.getItem("formSchema"));
     const isMissingSchema = !formSchema || formSchema.length === 0;
+    
     const submittedFormData = JSON.parse(localStorage.getItem("submittedFormData"));
+    const isMissingFormData = !submittedFormData || submittedFormData.length === 0;
+
+    console.log(isMissingFormData);
 
     const defaultLabelInFormSchema = formSchema?.find(schema =>
         defaultLabels.includes(schema.label)
     )?.label;
 
     useEffect(() => {
-        if (pathname === "/final-form") {
+        if (pathname === "/form") {
             if (isMissingSchema) {
                 toastNotification("Please create form schema before accessing final form page", "error");
             } else if (defaultLabelInFormSchema) {
                 toastNotification(`${defaultLabelInFormSchema} is not allowed label please change that`, "error");
             }
-        } else if (pathname === "/confirmation-page") {
-            if (!submittedFormData) {
+        } else if (pathname === "/list") {
+            if (isMissingFormData) {
                 toastNotification("Please fill the form before viewing confirmation page", "error");
             }
         }
-    }, [isMissingSchema, defaultLabelInFormSchema, pathname, submittedFormData]);
+    }, [isMissingSchema, defaultLabelInFormSchema, pathname, isMissingFormData]);
 
-    if (pathname === "/final-form" && (isMissingSchema || defaultLabelInFormSchema)) {
+    if (pathname === "/form" && (isMissingSchema || defaultLabelInFormSchema)) {
         return <Navigate to="/" replace />;
-    } else if (pathname === "/confirmation-page" && (!submittedFormData)) {
-        return <Navigate to="/final-form" replace />;
+    } else if (pathname === "/list" && isMissingFormData) {
+        return <Navigate to="/form" replace />;
     }
 
     return <Outlet />;
