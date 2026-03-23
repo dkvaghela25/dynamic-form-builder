580f6bb18563878cea9e46cddd3af486a2d1f5e8
Divyang Vaghela <dkvaghela724@gmail.com>
2026-03-23 14:42:28 +0530

    Changed empty message text when schema is empty

--- src/components/MainContent/FormContainer.jsx
+++ src/components/MainContent/FormContainer.jsx
@@ -25,7 +25,10 @@ const FormContainer = ({ editMode }) => {
     return (
         <div className="h-full" ref={setNodeRef}>
             {formSchema.length === 0
-                ? <div className="flex h-full w-full items-center justify-center rounded-xl border border-dashed border-slate-300 bg-slate-50 p-10 text-center text-2xl font-semibold text-slate-600">Select an input type from the left panel</div>
+                ? <div className="flex flex-col h-full w-full items-center justify-center rounded-xl border border-dashed border-slate-300 bg-slate-50 p-10 text-center text-2xl font-semibold text-slate-600">
+                    <div><span className="font-bold">Select</span> an input type from the left panel</div>
+                    <div>or <span className="font-bold">drag and drop</span> the field from left to this area</div>
+                </div>
                 : <FormProvider {...methods}>
                     <form className="h-[stretch] flex flex-col gap-4">
                         {editMode
